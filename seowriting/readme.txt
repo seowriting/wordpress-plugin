d51a45495951b0bac7a551af0167187b3c19e684
Vladimir Galunshchikov <soyayaos@gmail.com>
2024-04-08 14:32:09 +0200

    feat: The correct way to pass the author ID from the backend to the plugin

--- seowriting/readme.txt
+++ seowriting/readme.txt
@@ -4,7 +4,7 @@ Tags: seo writing, AI tool, AI writing, generation text
 Tested up to: 6.4
 Requires at least: 4.9
 Requires PHP: 5.6.20
-Stable tag: 1.5.3
+Stable tag: 1.5.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -21,6 +21,10 @@ SEOWriting’s powerful plugin allows you to seamlessly publish titles, texts, i
 The plugin uses the REST-API provided by [https://seowriting.ai/](https://seowriting.ai/?utm_source=wp_plugin). The Service is provided under the terms of [Terms of Service](https://seowriting.ai/terms-of-service?utm_source=wp_plugin) and [Privacy Policy](https://seowriting.ai/privacy-policy?utm_source=wp_plugin).
 
 == Changelog ==
+
+= 1.5.4 (2024/04/08) =
+
+Fix: The correct way to pass the author ID from the backend to the plugin
 
 = 1.5.3 (2024/04/08) =
 
--- seowriting/seowriting.php
+++ seowriting/seowriting.php
@@ -8,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name:       SEOWriting
  * Description:       SEOWriting - AI Writing Tool Plugin For Text Generation
- * Version:           1.5.3
+ * Version:           1.5.4
  * Author:            SEOWriting
  * Author URI:        https://seowriting.ai/?utm_source=wp_plugin
  * License:           GPL-2.0 or later
@@ -27,7 +27,7 @@ if (!class_exists('SEOWriting')) {
     {
         public $plugin_slug;
         public $plugin_path;
-        public $version = '1.5.3';
+        public $version = '1.5.4';
         /**
          * @var \SEOWriting\APIClient|null
          */
@@ -644,6 +644,9 @@ if (!class_exists('SEOWriting')) {
                 )
             );
             $data['html'] = $content;
+            if (isset($data['author_id'])) {
+                $user_id = (int)$data['author_id'];
+            }
             $new_post = [
                 'post_title' => sanitize_text_field($data['theme']),
                 'post_content' => $content,
