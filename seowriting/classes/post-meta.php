41218b8c16dbfa3998214b9ec0596366400f012b
Vladimir Galunshchikov <soyayaos@gmail.com>
2024-06-13 12:05:59 +0200

    feat: multiple keyword support in some plugins

--- seowriting/classes/post-meta.php
+++ seowriting/classes/post-meta.php
@@ -15,12 +15,14 @@ class PostMeta
     private $meta_keys = null;
 
     const PLUGIN_ALL_IN_ONE = 'all-in-one-seo-pack/all_in_one_seo_pack.php';
+    const PLUGIN_ALL_IN_ONE_PRO = 'all-in-one-seo-pack-pro/all_in_one_seo_pack.php';
     const PLUGIN_ELEMENTOR = 'elementor/elementor.php';
     const PLUGIN_RANK_MATH = 'seo-by-rank-math/rank-math.php';
     const PLUGIN_SEO_FRAMEWORK = 'autodescription/autodescription.php';
     const PLUGIN_SEOPRESS = 'wp-seopress/seopress.php';
     const PLUGIN_SQUIRRLY_SEO = 'squirrly-seo/squirrly.php';
     const PLUGIN_YOAST = 'wordpress-seo/wp-seo.php';
+    const PLUGIN_YOAST_PRO = 'wordpress-seo-premium/wp-seo-premium.php';
 
     public function __construct($post_id)
     {
@@ -60,6 +62,8 @@ class PostMeta
         $title = isset($data['title']) ? $data['title'] : '';
         $description = isset($data['description']) ? $data['description'] : '';
         $main_keyword = isset($data['main_keyword']) ? $data['main_keyword'] : '';
+        $keywords = isset($data['keywords']) ? $data['keywords'] : [];
+        $all_keywords = count($keywords) > 0 ? implode(',', $keywords) : $main_keyword;
 
         if (
             is_plugin_active(self::PLUGIN_ELEMENTOR)
@@ -139,13 +143,21 @@ class PostMeta
             update_post_meta($this->post_id, '_elementor_data', seowriting_json_encode_unescaped($elementorSettings));
         }
 
-        if (is_plugin_active(self::PLUGIN_YOAST)) {
+        if (is_plugin_active(self::PLUGIN_YOAST) || is_plugin_active(self::PLUGIN_YOAST_PRO)) {
             $this->setValue('_yoast_wpseo_title', $title);
             $this->setValue('_yoast_wpseo_metadesc', $description);
             $this->setValue('_yoast_wpseo_focuskw', $main_keyword);
+            $list = [];
+            foreach(array_slice($keywords, 1) as $item) {
+                $list[] = [
+                    'keyword' => $item,
+                    'score' => 0,
+                ];
+            }
+            $this->setValue('_yoast_wpseo_focuskeywords', seowriting_json_encode_unescaped($list));
         }
 
-        if (is_plugin_active(self::PLUGIN_ALL_IN_ONE)) {
+        if (is_plugin_active(self::PLUGIN_ALL_IN_ONE) || is_plugin_active(self::PLUGIN_ALL_IN_ONE_PRO)) {
             $this->setValue('_aioseo_title', $title);
             $this->setValue('_aioseo_description', $description);
             $this->setValue('_aioseo_keywords', $main_keyword);
@@ -153,8 +165,15 @@ class PostMeta
             $this->setValue('_aioseo_og_description', $description);
 
             try {
-                $path = WP_PLUGIN_DIR . '/' . self::PLUGIN_ALL_IN_ONE;
+                $path = WP_PLUGIN_DIR . '/' . (is_plugin_active(self::PLUGIN_ALL_IN_ONE) ? self::PLUGIN_ALL_IN_ONE : self::PLUGIN_ALL_IN_ONE_PRO);
                 include_once($path);
+                $list = [];
+                foreach(array_slice($keywords, 1) as $item) {
+                    $list[] = [
+                        'keyphrase' => $item,
+                        'analysis' => [],
+                    ];
+                }
                 // @phpstan-ignore-next-line
                 \AIOSEO\Plugin\Common\Models\Post::savePost($this->post_id, [
                     'title' => $title,
@@ -165,7 +184,7 @@ class PostMeta
                             'keyphrase' => $main_keyword,
                             'analysis' => [],
                         ],
-                        'additional' => [],
+                        'additional' => $list,
                     ],
                     'og_title' => $title,
                     'og_description' => $description,
@@ -178,13 +197,13 @@ class PostMeta
         if (is_plugin_active(self::PLUGIN_RANK_MATH)) {
             $this->setValue('rank_math_title', $title);
             $this->setValue('rank_math_description', $description);
-            $this->setValue('rank_math_focus_keyword', $main_keyword);
+            $this->setValue('rank_math_focus_keyword', $all_keywords);
         }
 
         if (is_plugin_active(self::PLUGIN_SEOPRESS)) {
             $this->setValue('_seopress_titles_title', $title);
             $this->setValue('_seopress_titles_desc', $description);
-            $this->setValue('_seopress_analysis_target_kw', $main_keyword);
+            $this->setValue('_seopress_analysis_target_kw', $all_keywords);
             $this->setValue('_seopress_social_fb_title', $title);
             $this->setValue('_seopress_social_fb_desc', $description);
             $this->setValue('_seopress_social_twitter_title', $title);
